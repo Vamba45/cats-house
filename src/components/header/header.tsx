6f132ba091c089dccd944d2d183605b24040d8f8
Vamba45 <vambafool@bk.ru>
2024-12-12 22:42:32 +0300

    add header style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,22 @@
 import  { React, FC } from 'react';
 import './header.scss';
+import logo from '../../assets/logo.png';
 
 export const Header : FC = () => {
     return (
         <header className="header">
-            <div className="header__row">
-                <div className="header__column header__logo">
-                    <a href="/">logo</a>
-                </div>
-                <div className="header__column">
-                    <a href="/login" className="header__login">вход</a>
-                    <a href="/registration" className="header__registration">регистрация</a>
+            <div className="container">
+                <div className="header__row">
+                    <div className="header__column">
+                        <a className="header__logo" href="/">
+                            <img src={logo} alt="logo-header" />
+                            <p>Кошкин дом</p>
+                        </a>
+                    </div>
+                    <div className="header__column">
+                        <a href="/login" className="header__login">вход</a>
+                        <a href="/registration" className="header__registration">регистрация</a>
+                    </div>
                 </div>
             </div>
         </header>
