d38fd87bb2d8f753016d9aa5145cbc223d77a06b
Vamba45 <vambafool@bk.ru>
2024-12-13 15:20:57 +0300

    add cards output to main page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,14 +1,22 @@
 import { Card } from '../../components/card/card.tsx';
 import { Search } from '../../components/search/search.tsx';
 import './main.scss';
-import { FC, React } from 'react';
+import { FC, React, useState, useEffect } from 'react';
 import img from '../../assets/logo.png';
 
+
 export const Main : FC = () => {
+
+    useEffect(() => {
+        
+    }, [])
+
+    const [state, setState] = useState();
+
     return(
         <div className="mainpage">
            <div className="container">
-                <Search/>
+                <Search value={state} onChange={(e) => { setState(e.target.value); console.log(state) }}/>
 
                 <div className="mainpage__grid">
                     <Card text={"Dog"} img={img}/>
